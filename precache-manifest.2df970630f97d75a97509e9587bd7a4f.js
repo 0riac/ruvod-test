1ca15e57ff8e4a4d99ad20377d5041e5364c2f77
0riac <katan998@mail.ru>
2020-06-28 13:04:00 +0300

    fix path

---
+++ precache-manifest.2df970630f97d75a97509e9587bd7a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37cf1087be43d1c6427ef5505b8614b",
+    "url": "/ruvod-test/index.html"
+  },
+  {
+    "revision": "37f2d18af84d0dc23278",
+    "url": "/ruvod-test/static/css/main.c942d61e.chunk.css"
+  },
+  {
+    "revision": "a3da9fa99113329e1193",
+    "url": "/ruvod-test/static/js/2.b5c15413.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/ruvod-test/static/js/2.b5c15413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f2d18af84d0dc23278",
+    "url": "/ruvod-test/static/js/main.b93e3d8e.chunk.js"
+  },
+  {
+    "revision": "30b3249b1c43017c4736",
+    "url": "/ruvod-test/static/js/runtime-main.ebe8db32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d50599fd1cd4704a4e24ac0f902ab08.js"
+  "/ruvod-test/precache-manifest.2df970630f97d75a97509e9587bd7a4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ruvod-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
