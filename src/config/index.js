fe07eef552489dd4376cf6363620492ba4515d81
0riac <katan998@mail.ru>
2020-06-29 00:48:55 +0300

    fix: fix api endpoint

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const constants = {
   TOKEN_STORAGE_KEY: 'token',
-  API_ENDPOINT: 'http://localhost:4010',
+  API_ENDPOINT: 'https://ruvodtest.herokuapp.com',
   GITHUB_CLIENT_ID: 'ee99345152f8eba1dfc0',
   ROUTING_SUBPATH: '/ruvod-test'
 };
